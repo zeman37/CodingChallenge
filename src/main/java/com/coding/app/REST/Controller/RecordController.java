715c7e2a714abe2fb0a7ccc42d730500c0c8c110
zeman37 <zemaitis.mantas99@gmail.com>
2022-07-19 23:16:46 +0300

    Implemented .csv file export functionality, tested data export from database.

--- src/main/java/com/coding/app/REST/Controller/RecordController.java
+++ src/main/java/com/coding/app/REST/Controller/RecordController.java
@@ -2,14 +2,20 @@ package com.coding.app.REST.Controller;
 
 import com.coding.app.REST.Models.Record;
 import com.coding.app.REST.Models.RecordRepo;
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -33,5 +39,25 @@ public class RecordController {
         }
         service.saveAll(records);
         return "Upload Successful";
+    }
+
+
+    @GetMapping("/export")
+    public void exportCSV(HttpServletResponse response) throws Exception {
+
+        String filename = "monetary.csv";
+
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"");
+
+        StatefulBeanToCsv<Record> writer = new StatefulBeanToCsvBuilder<Record>(response.getWriter())
+                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                .withSeparator(CSVWriter.DEFAULT_SEPARATOR)
+                .withOrderedResults(false)
+                .build();
+
+        writer.write(service.findAll());
+
     }
 }
--- src/main/java/com/coding/app/REST/Models/RecordRepo.java
+++ src/main/java/com/coding/app/REST/Models/RecordRepo.java
@@ -3,7 +3,9 @@ package com.coding.app.REST.Models;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecordRepo extends JpaRepository<Record, Integer> {
-
+    List<Record> findAll();
 }
